54e1679e25627c2f85bb150605ab5479e2bbe89a
amiyo-bhowmik <amiyobhowmik@gmail.com|>
2026-03-28 17:50:38 +0600

    Initial Commit

---
+++ src/main/java/com/thesisplatform/ThesisplatformApplication.java
@@ -0,0 +1,13 @@
+package com.thesisplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesisplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThesisplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thesisplatform/ThesisplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thesisplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesisplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
