41e666abd15bd49a25c884009b2257da527760fc
amiyo-bhowmik <amiyobhowmik123@gmail.com>
2026-04-21 22:06:30 +0600

    feat: wire login mode, redirects, and token storage

--- src/main/java/com/example/ThesisConnect/web/ApiExceptionHandler.java
+++ src/main/java/com/example/ThesisConnect/web/ApiExceptionHandler.java
@@ -1,15 +1,17 @@
 package com.example.ThesisConnect.web;
 
-import java.util.Map;
-
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
+
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<Map<String, String>> handleResponseStatusException(ResponseStatusException exception) {
         HttpStatus status = HttpStatus.valueOf(exception.getStatusCode().value());
@@ -22,5 +24,11 @@ public class ApiExceptionHandler {
     ) {
         return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(Map.of("message", "Email is already taken"));
-    }    
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleException(Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Map.of("message", "Something went wrong. Please try again."));
+    }
 }
